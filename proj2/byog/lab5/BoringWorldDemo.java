dcc56f4a3d7a53ab0d2c733b92b0ff30bc9421ef
TrSunOwen <tianren.sun@outlook.com>
2021-06-16 13:04:09 +0200

    update

--- proj2/byog/lab5/BoringWorldDemo.java
+++ proj2/byog/lab5/BoringWorldDemo.java
@@ -8,8 +8,8 @@ import byog.TileEngine.Tileset;
  *  Draws a world that is mostly empty except for a small region.
  */
 public class BoringWorldDemo {
-    private static final int WIDTH = 60;
-    private static final int HEIGHT = 30;
+    private static final int WIDTH = 60; // x
+    private static final int HEIGHT = 30; // y
 
     public static void main(String[] args) {
         // initialize the tile rendering engine with a window of size WIDTH x HEIGHT
@@ -20,14 +20,15 @@ public class BoringWorldDemo {
         TETile[][] world = new TETile[WIDTH][HEIGHT];
         for (int x = 0; x < WIDTH; x += 1) {
             for (int y = 0; y < HEIGHT; y += 1) {
-                world[x][y] = Tileset.NOTHING;
+                world[x][y] = Tileset.NOTHING; // the black background
             }
         }
 
         // fills in a block 14 tiles wide by 4 tiles tall
         for (int x = 20; x < 35; x += 1) {
             for (int y = 5; y < 10; y += 1) {
-                world[x][y] = Tileset.WALL;
+                // fills the pattern you like, e.g. wall, flower or grass
+                world[x][y] = Tileset.WALL; // the wall pattern filled in the background
             }
         }
 
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,10 +6,12 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+
 }
